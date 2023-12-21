5bc9bb011b2014d7ffda748282bc14df28514126
unknown <m.r.fouladi7@gmail.com>
2023-12-21 10:12:39 +0330

    authentication controll

---
+++ src/controllers/authentication.ts
@@ -0,0 +1,35 @@
+import { createUser, getUserByEmail } from "db/users";
+import express from "express";
+import { authentication, random } from "helpers";
+
+const register = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password, username } = req.body;
+
+    if (!email || !password || !username) {
+      return res.sendStatus(400);
+    }
+
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+      return res.sendStatus(400);
+    }
+
+    const salt = random();
+    const user = await createUser({
+      email,
+      username,
+      password,
+      authentication: {
+        salt,
+        password: authentication(salt, password),
+      },
+    });
+
+    return res.status(201).json(user).end();
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
+};
